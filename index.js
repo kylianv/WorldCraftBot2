9e525cc100f56be63dcb665870c53b45b5abfc46
corentinlb07 <35633930+corentinlb07@users.noreply.github.com>
2018-01-24 21:55:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.on('message', message => {
         var help_embed = new Discord.RichEmbed()
         .setColor('#D9F200')
         .addField("Commandes du bot !", " - wc!help : Affiche les commandes du bot !\n- wc!youtube : Le lien de notre chaîne YouTube !\n- wc!paypal : Vous donne le paypal de WorldCraft !\n- wc!maj : Il vous donne les mises à jour de la semaine !")
-        .addField("Commandes divers !", " - ping : Le bot répond pong !\n- test : Le bot vous répond !\n- Insultez le, il vous instule !\n #grosvent : Il se fout de vous :wink: !")
+        .addField("Commandes divers !", " - ping : Le bot répond pong !\n- test : Le bot vous répond !\n- Insultez le, il vous instule !\n- #grosvent : Il se fout de vous :wink: !")
         .setFooter("Cordialement, WorldCraftBot")
         message.channel.send(help_embed);
         console.log("Commande Help demandée !");
