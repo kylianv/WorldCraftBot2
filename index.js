894c79db9cb27415216e3229a8d7ec1014bd2ea8
corentinlb07 <35633930+corentinlb07@users.noreply.github.com>
2018-01-24 18:46:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,158 @@
+const Discord = require('discord.js');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter= new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [], xp: []}).write()
+
+var bot = new Discord.Client();
+var prefix = ("$");
+var randnum = 0;
+
+bot.on('ready', () => {
+    bot.user.setPresence({game: { name: '[$help] Bot Officiel de WorldCraft', type: 0}})
+    console.log("Bot Ready !");
+});
+
+bot.login('Mzk2NDUyMTIzMDAyMjczNzky.DShoWA.CoGNsqQIl1mfuaT6TcPT-zchkRw');
+
+bot.on('message', message => {
+
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+    if(!db.get("xp").find({user: msgauthor}).value()){
+        db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb)
+        var userxp = Object.values(userxpdb)
+        console.log(userxp);
+        console.log(`Nombre d'xp : ${userxp[1]}`)
+
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+    }
+    
+
+    if (message.content === "ping"){
+        message.reply("pong");
+        console.log('ping pong')
+    }
+    
+    if (!message.content.startsWith(prefix)) return;
+    var args = message.content.substring(prefix.length).split(" ");
+
+    switch (args[0].toLowerCase()){
+
+        case "newstory":
+        var value = message.content.substr(10);
+        var author = message.author;
+        var number = db.get('histoires').map('id').value();
+        console.log(value);
+        message.reply("Ajout de l'histoire à la base de données")
+
+        db.get('histores')
+            .push({ story_value: value, story_author: author}).write()
+        
+        break;
+    }
+
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#D9F200')
+        .addField("Commandes du bot !", " - $help : Affiche les commandes du bot !")
+        .addField("Commandes divers !", " - ping : Le bot répond pong !")
+        .setFooter("Cordialement, WorldCraftBot")
+        message.channel.sendEmbed(help_embed);
+        console.log("Commande Help demandée !");
+    }
+
+    if (message.content === prefix + "youtube"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#25c059')
+        .addField("**Salut!**", "Voici le lien de la chaine youtube : <https://www.youtube.com/channel/UCd8YB_jkEMrKDTyI2f2Eahw>")
+        .setFooter("Merci de vous abonner à la chaine ! :wink:");
+        message.channel.sendEmbed(help_embed);
+        console.log("Chaîne YouTube demandé !");
+    }
+
+    if (message.content === prefix + "Youtube"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#25c059')
+        .addField("**Salut!**", "Voici le lien de la chaine youtube : <https://www.youtube.com/channel/UCd8YB_jkEMrKDTyI2f2Eahw>")
+        .setFooter("Merci de vous abonner à la chaine ! :wink:");
+        message.channel.sendEmbed(help_embed);
+        console.log("Chaîne YouTube demandé !");
+    }
+
+    if (message.content === prefix + "YouTube"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#25c059')
+        .addField("**Salut!**", "Voici le lien de la chaine youtube : <https://www.youtube.com/channel/UCd8YB_jkEMrKDTyI2f2Eahw>")
+        .setFooter("Merci de vous abonner à la chaine ! :wink:");
+        message.channel.sendEmbed(help_embed);
+        console.log("Chaîne YouTube demandé !");
+    }
+
+    if (message.content === prefix + "yt"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#25c059')
+        .addField("**Salut!**", "Voici le lien de la chaine youtube : <https://www.youtube.com/channel/UCd8YB_jkEMrKDTyI2f2Eahw>")
+        .setFooter("Merci de vous abonner à la chaine ! :wink:");
+        message.channel.sendEmbed(help_embed);
+        console.log("Chaîne YouTube demandé !");
+    }
+
+    if (message.content === prefix + "YT"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#25c059')
+        .addField("**Salut!**", "Voici le lien de la chaine youtube : <https://www.youtube.com/channel/UCd8YB_jkEMrKDTyI2f2Eahw>")
+        .setFooter("Merci de vous abonner à la chaine ! :wink:");
+        message.channel.sendEmbed(help_embed);
+        console.log("Chaîne YouTube demandé !");
+    }
+
+    if (message.content === prefix + "Yt"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#25c059')
+        .addField("**Salut!**", "Voici le lien de la chaine youtube : <https://www.youtube.com/channel/UCd8YB_jkEMrKDTyI2f2Eahw>")
+        .setFooter("Merci de vous abonner à la chaine ! :wink:");
+        message.channel.sendEmbed(help_embed);
+        console.log("Chaîne YouTube demandé !");
+    }
+
+    if (message.content === "Comment vas-tu WorldCraftBot?"){
+        random();
+
+        if (randnum == 1){
+            message.reply("Merci je vais très bien !");
+            console.log(randnum);
+        }
+
+        if (randnum == 2){
+            message.reply("Je ne vais pas très bien, merci de te soucier de moi !");
+            console.log(randnum);
+        }
+
+    }
+
+    if (message.content === prefix + "xpstat"){
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle(`XP de ${message.author.username}`)
+            .setDescription("Voici tout vos xp monsieur !")
+            .addField("XP :", `${xpfinal[1]}xp`)
+        message.channel.send({embed: xp_embed});
+    }
+
+});
+
+function random(min, max) {
+    min = Math.ceil(0);
+    max = Math.floor(2);
+    randnum = Math.floor(Math.random() * (max - min +1) + min);
+}
