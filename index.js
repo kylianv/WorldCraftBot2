32fe4587292693eccc28313cd6b4031464eeb9bb
corentinlb07 <35633930+corentinlb07@users.noreply.github.com>
2018-01-30 21:08:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
         .setColor('#D9F200')
-        .addField("Commandes du bot !", " - wc!help : Affiche les commandes du bot !\n- wc!helpmp : Affiche les commandes du bot en message privé !\n- wc!youtube : Le lien de notre chaîne YouTube !\n- wc!paypal : Vous donne le paypal de WorldCraft !\n- wc!maj : Il vous donne les mises à jour de la semaine !\n- wc!info : Vous donnes des informations sur vous !\n wc!infode @user : Vous donnes des informations sur la personnes de votre choix ! **(Pas encore disponible)**")
+        .addField("Commandes du bot !", " - wc!help : Affiche les commandes du bot !\n- wc!helpmp : Affiche les commandes du bot en message privé !\n- wc!youtube : Le lien de notre chaîne YouTube !\n- wc!paypal : Vous donne le paypal de WorldCraft !\n- wc!maj : Il vous donne les mises à jour de la semaine !\n- wc!info : Vous donnes des informations sur vous !\n- wc!infode @user : Vous donnes des informations sur la personnes de votre choix ! **(Pas encore disponible)**\n- wc!xpstat : Vous donnes votre xp !")
         .addField("Commandes divers !", " - ping : Le bot répond pong !\n- test : Le bot vous répond !\n- #grosvent : Il se fout de vous  !")
         .setFooter("Cordialement, WorldCraftBot")
         message.channel.send(help_embed);
@@ -45,7 +45,7 @@ bot.on('message', message => {
     if (message.content === prefix + "helpmp"){
         var help_embed = new Discord.RichEmbed()
         .setColor('#D9F200')
-        .addField("Commandes du bot !", " - wc!help : Affiche les commandes du bot !\n- wc!helpmp : Affiche les commandes du bot en message privé !\n- wc!youtube : Le lien de notre chaîne YouTube !\n- wc!paypal : Vous donne le paypal de WorldCraft !\n- wc!maj : Il vous donne les mises à jour de la semaine !\n- wc!info : Vous donnes des informations sur vous !\n wc!infode @user : Vous donnes des informations sur la personnes de votre choix ! **(Pas encore disponible)**")
+        .addField("Commandes du bot !", " - wc!help : Affiche les commandes du bot !\n- wc!helpmp : Affiche les commandes du bot en message privé !\n- wc!youtube : Le lien de notre chaîne YouTube !\n- wc!paypal : Vous donne le paypal de WorldCraft !\n- wc!maj : Il vous donne les mises à jour de la semaine !\n- wc!info : Vous donnes des informations sur vous !\n- wc!infode @user : Vous donnes des informations sur la personnes de votre choix ! **(Pas encore disponible)**\n- wc!xpstat : Vous donnes votre xp !")
         .addField("Commandes divers !", " - ping : Le bot répond pong !\n- test : Le bot vous répond !\n- #grosvent : Il se fout de vous :wink: !")
         .setFooter("Cordialement, WorldCraftBot")
         message.author.send(help_embed);
@@ -63,6 +63,18 @@ bot.on('message', message => {
         message.author.send(help_embed);
         console.log("Commande Info demandé");
     }
+	
+   if (message.content === prefix + "xpstat"){
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+        .setTitle(`XP de ${message.author.username}`)
+        .setDescription("Voici tout vos xp !")
+        .addField("XP :", `${xpfinal[1]}xp`)
+	.setThumbnail(message.author.avatarURL)
+        message.channel.send({embed: xp_embed});
+	console.log("Commande xpstat demandé");
+   }
     
 
     if (message.content === "ping"){
