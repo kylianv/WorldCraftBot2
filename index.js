2a73d6157796923439fb1be1596f72375d1be16b
albertgerard <33197223+albertgerard@users.noreply.github.com>
2018-02-12 09:12:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,64 +37,21 @@ member.kick()
 ) 
 
 bot.on('message', function(message){
-	if (message.content === prefix + "kickbot" )  {
-
-
-
+if (message.content === prefix + "kickbotoff" )  {
     if ( message.author.id === '292263751372242944' || message.author.id === '394255380940849153' || message.author.id === '335118921088630796' || message.author.id === '270265203269042188' || message.author.id === '283625708863881216' || message.author.id === '205752580251451392' || message.author.id === '344061192903327744' ) {
-
-
-
     if (kickbot === 1) {
-
-
-
-            const kickbot = 0 
-
-
-	    
-	
-
-            message.channel.send('Le kickbot est bien désactivé') } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+       const kickbot = 0
+       message.channel.send('Le kickbot est bien désactivé') } 
     } else {
-
-
-
             message.channel.send('Tu n\' es pas autorisé a faire cette commande')
-
-
-
-} 
-
-
-
-        
-
-
-
-    } 
-if (message.content === prefix + "kickbot"){
- if ( message.author.id === '292263751372242944' || message.author.id === '394255380940849153' || message.author.id === '335118921088630796' || message.author.id === '270265203269042188' || message.author.id === '283625708863881216' || message.author.id === '205752580251451392' || message.author.id === '344061192903327744' ) { 
-if (kickbot === 0) {
-	let kickbot= 1
-	message.channel.send('Le kickot est désormais activé') } 
-	
-} }} ) 
+} } 
+if (message.content === prefix + "kickboton"){
+if ( message.author.id === '292263751372242944' || message.author.id === '394255380940849153' || message.author.id === '335118921088630796' || message.author.id === '270265203269042188' || message.author.id === '283625708863881216' || message.author.id === '205752580251451392' || message.author.id === '344061192903327744' ) { 
+	let kickbot = 1
+	message.channel.send('Le kickot est désormais activé')}
+} else {
+    message.channel.send('Tu n\' es pas autorisé a faire cette commande')}	
+} ) 
 	
 bot.on('message', message => {
     	const args = message.content.slice(prefix.length).trim().split(/ wc!/g)
@@ -113,21 +70,7 @@ bot.on('message', message => {
         console.log(`Nombre d'xp : ${userxp[1]}`)
 
         db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-}
- /*   if (message.content === prefix + "kickbot" )  {b
-    if ( message.author.id === '292263751372242944' || message.author.id === '394255380940849153' || message.author.id === '335118921088630796' || message.author.id === '270265203269042188' || message.author.id === '283625708863881216' || message.author.id === '205752580251451392' || message.author.id === '344061192903327744' ) {
-    if (kickbot === 1) {
-	    const kickbot = 0 
-	    message.channel.send('Le kickbot est bien désactivé' ) }
-    if ( kickbot === 0 )  { 
-		const kickbot = 1
-*/	
-
-
-
-	
-    
-        
+} 	     
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
         .setColor('#12a0aa')
@@ -140,8 +83,8 @@ bot.on('message', message => {
 	.setFooter("Cordialement, WorldCraftBot")
 	message.channel.send(help_embed2);
 	var help_embed3 = new Discord.RichEmbed()
-	.setColor('#ff0000')
-	.addField("Commandes réservé au staff !", "- wc!suggest Votre message : Disponible uniquement dans <#348545352904998914> !")
+	.setColor('#ff0000') 
+	.addField("Commandes réservé au staff !", "- wc!suggest Votre message : Disponible uniquement dans <#348545352904998914> !\n-wc!kickboton/wc!kickbotoff : Active/désactive le kickbot (seulement les Co-Fondateurs et le Fondateur)")
 	.setFooter("Cordialement, WorldCraftBot")
 	message.channel.send(help_embed3);
         console.log("Commande Help demandée !");
