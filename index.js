851ed996332c1febbfd1ce7d6b0bd8c90efbf639
albertgerard <33197223+albertgerard@users.noreply.github.com>
2018-02-13 12:55:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const express = require('express');
 const app = express();
-const kickbot = 1
+const kickbot = 2
 const adapter= new FileSync('database.json');
 const db = low(adapter);
 
@@ -30,7 +30,7 @@ bot.on('ready', () => {
 bot.login(process.env.TOKEN);
 
 bot.on('guildMemberAdd', function(member) {
-if ( member.user.bot === true && kickbot === 1) {
+if ( member.user.bot === true && kickbot === 2) {
 member.kick()
 }
 }
@@ -39,14 +39,14 @@ member.kick()
 bot.on('message', function(message){
 if (message.content === prefix + "kickbotoff" )  {
     if ( message.author.id === '292263751372242944' || message.author.id === '394255380940849153' || message.author.id === '335118921088630796' || message.author.id === '270265203269042188' || message.author.id === '283625708863881216' || message.author.id === '205752580251451392' || message.author.id === '344061192903327744' ) {
-       const kickbot = 0
+       const kickbot = 1
        message.channel.send('Le kickbot est bien désactivé')
     } else {
             message.channel.send('Tu n\' es pas autorisé a faire cette commande')
 } } 
 if (message.content === prefix + "kickboton"){
 if ( message.author.id === '292263751372242944' || message.author.id === '394255380940849153' || message.author.id === '335118921088630796' || message.author.id === '270265203269042188' || message.author.id === '283625708863881216' || message.author.id === '205752580251451392' || message.author.id === '344061192903327744' ) { 
-	let kickbot = 1
+	let kickbot = 2
 	message.channel.send('Le kickot est désormais activé')
 } else {
 	message.channel.send('Tu n\' es pas autorisé a faire cette commande')}
